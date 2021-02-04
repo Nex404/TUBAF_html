f5dfd36704c5dc89615bf32b37160c332f2d757e
Dennys-Daniel <dans.vogt@gmail.com>
2021-02-04 15:28:38 +0100

    fertige Übung

--- Aufgabe3/lösung.py
+++
@@ -1,8 +0,0 @@
-name = input()
-print("<html><head><meta charset='utf-8'/>")
-print(f"<title>Persönliche Website von {name} </title></head>")
-print("""<body>
-        <h1>Willkommen auf meiner Seite</h1>
-        Dies ist meine persönliche Seite.<br>""")
-print(f"mein name ist {name}")
-print("</body></html>")
